b08db937df9e3589d1edf969d3aa530905703198
leeun <Nhunglee.221@gmail.com>
2025-10-28 14:14:50 +0700

    constant category style

---
+++ src/constants/categoryStyles.ts
@@ -0,0 +1,10 @@
+import { CategoryType } from '@/types/product'
+
+// Construct a type with a set of properties K of type T
+export const categoryStyles: Record<CategoryType, string> = {
+  'Pure Brilliance': 'rgb(241, 204, 207)',
+  'Varnaya Blends': 'rgb(216, 208, 196)',
+  'Daily Dew': 'rgb(208, 200, 223)',
+  'Clear Difference': 'rgb(194, 205, 222)',
+  'C Luminance': 'rgb(243, 215, 175)'
+}
