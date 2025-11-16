23c6917286406a1262126cc1c9edc9bb23a6a2fb
leeun <Nhunglee.221@gmail.com>
2025-11-16 22:58:19 +0700

    config project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
   images: {
     remotePatterns: [
       {
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   experimental: {
+    globalNotFound: true,
+  },
+  // reactStrictMode: false
 };
 
 export default nextConfig;
