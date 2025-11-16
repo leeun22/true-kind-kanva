d4e19a8d2a2f3fb61994a607655dfb5f727ba9e4
leeun <Nhunglee.221@gmail.com>
2025-11-17 00:28:48 +0700

    Update responsive ingredient component

--- src/components/ingredient/IngredientCard.tsx
+++ src/components/ingredient/IngredientCard.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from 'next/image'
+import styles from './ingredient.module.css'
 
 interface IngredientCardProps {
   icon: string
@@ -16,9 +17,9 @@ interface IngredientCardProps {
 export default function IngredientCard({ icon, title, description, pos }: IngredientCardProps) {
   return (
     <div
-      className={`ingredients__item w-full max-w-[256px] bg-[#f5f5f5] rounded-[14px] pt-[51px] pb-[38px] px-[32px] text-center ${pos?.mr} ${pos?.mt}`}
+      className={`${styles.ingredientsItem} w-full xl:max-w-[256px] min-[1025px]:max-w-[200px] md:max-w-[175px] max-w-full md:bg-[#f5f5f5] md:rounded-[14px] xl:p-[51px_32px_38px] md:p-[34px_21px_25px] min-[550px]:px-[20px]  text-center ${pos?.mr} ${pos?.mt}`}
     >
-      <div className="ingredients__item-icon flex items-center justify-center bg-white rounded-full w-[80px] h-[80px] p-[12px] m-auto">
+      <div className="ingredients__item-icon flex items-center justify-center md:bg-white bg-[#f5f5f5] rounded-full xl:w-[80px] xl:h-[80px] xl:p-[12px] md:w-[58px] md:h-[58px] max-md:w-[78px] max-md:h-[78px] p-[8px] m-auto">
         <div className="media-img relative w-full">
           <Image
             className="w-full h-full min-h-[38px] max-h-[51px] object-cover"
@@ -32,11 +33,13 @@ export default function IngredientCard({ icon, title, description, pos }: Ingred
         </div>
       </div>
 
-      <h3 className="ingredients__item-title font-figtree text-[1.53rem] font-[600] leading-[1.1] tracking-[0] text-center max-w-[60%] my-[25px] mx-auto">
+      <h3 className="ingredients__item-title font-figtree md:text-fluid4lg text-[1.6rem] font-[600] leading-[1.1] tracking-[0] text-center md:max-w-[60%] max-w-[72%] xl:my-[25px] my-[16px] mx-auto">
         {title}
       </h3>
 
-      <p className="ingredients__item-desc font-figtree text-[1.28rem] font-[400] text-center">{description}</p>
+      <p className="ingredients__item-desc font-figtree md:text-fluid3lg text-[1.4rem] max-[550px]:hidden text-center">
+        {description}
+      </p>
     </div>
   )
 }
--- src/components/ingredient/index.tsx
+++ src/components/ingredient/index.tsx
@@ -12,7 +12,7 @@ export default function Ingredient() {
   const ref = useRef(null)
   const createTransformY = (y: number) => `translate3d(0px, ${y}%, 0px)`
 
-  // useInView: Chỉ trigger animation khi component vào viewport
+  // useInView: Only trigger animation when component enters viewport
   const isInView = useInView(ref, {
     once: true,
     margin: '-50px 0px -50px 0px'
@@ -23,7 +23,7 @@ export default function Ingredient() {
     offset: ['start 0.2', 'end 0.2']
   })
 
-  // Tạo tất cả transforms từ 1 scrollYProgress: Biến đổi transform một MotionValue thành MotionValue khác: useTransform(input, inputRange, outputRange) -> Framer Motion sẽ map giá trị scroll trong khoảng inputRange. VD: Khi scrollYProgress = 0 thì transform = createLeafTransform(110), khi scrollYProgress = 1 thì transform = createLeafTransform(-110)
+  // Create all transforms from 1 scrollYProgress: Transform one MotionValue to another MotionValue
   const leafTransform = useTransform(scrollYProgress, [-1, 1], [createTransformY(110), createTransformY(-93)])
 
   const empressTransform = useTransform(scrollYProgress, [-1, 1], [createTransformY(110), createTransformY(40)])
@@ -38,7 +38,7 @@ export default function Ingredient() {
 
   const card4Transform = useTransform(scrollYProgress, [-1, 1], [createTransformY(66), createTransformY(-6)])
 
-  // Object chứa tất cả transforms để dễ dàng truy cập theo index
+  // Object contains all transforms for easy access by index
   const transforms = {
     card1: card1Transform,
     card2: card2Transform,
@@ -47,9 +47,13 @@ export default function Ingredient() {
   }
 
   return (
-    <section ref={ref} id="ingredients" className="section block relative my-[115px]">
-      <div className="container relative max-w-[1400px] flex flex-col w-full m-auto px-[calc(8px*.5)]">
-        <div className="ingredients__text relative mb-[102px]">
+    <section
+      ref={ref}
+      id="ingredients"
+      className="section block relative md:my-[115px] min-[490px]:my-[80px] my-[62px]"
+    >
+      <div className="container relative flex flex-col w-full max-w-full">
+        <div className="ingredients__text relative md:mb-[102px] mb-[16px] max-w-container">
           <motion.div
             className="ingredients__title relative"
             variants={motionTextVariants.staggerContainer}
@@ -57,7 +61,7 @@ export default function Ingredient() {
             animate={isInView ? 'visible' : 'hidden'}
             transition={{ duration: 1.2, ease: 'linear' }}
           >
-            <h2 className="masking-text block font-figtree text-start text-[5.6rem] font-[500] leading-[1.1] -tracking-[.5px] overflow-hidden">
+            <h2 className="masking-text block font-figtree text-start text-fluid8xl font-[600] leading-[1.1] -tracking-[.5px] overflow-hidden">
               <motion.span className="word inline-block will-change-transform" variants={motionTextVariants.fadeInUp}>
                 CLEAN,
               </motion.span>{' '}
@@ -66,16 +70,16 @@ export default function Ingredient() {
               </motion.span>
             </h2>
 
-            <h2 className="masking-text block text-start font-figtree text-[5.6rem] font-[500] leading-[1.1] -tracking-[.5px] overflow-hidden">
+            <h2 className="masking-text block text-start font-figtree text-fluid8xl font-[600] leading-[1.1] -tracking-[.5px] overflow-hidden">
               <motion.span className="word inline-block will-change-transform" variants={motionTextVariants.fadeInUp}>
                 PERFORMANCE
               </motion.span>
             </h2>
 
-            <div className="masking-text absolute left-[24%] top-[128px] min-w-[295px] min-h-[88px] overflow-hidden">
+            <div className="masking-text md:absolute relative lg:left-[24%] md:left-[32%] left-0 xl:top-[128px] lg:top-[115px] md:top-[90px] top-0 min-w-[295px] xl:min-h-[88px] overflow-hidden">
               <div className="relative inline-block">
                 <motion.h2
-                  className="font-sentient font-[400] text-[8rem] italic leading-[1.1] -tracking-[5px] will-change-transform"
+                  className="font-sentient text-fluid6xl italic leading-[1.1] -tracking-[5px] will-change-transform"
                   variants={motionTextVariants.fadeInUp}
                   transition={{ duration: 0.3, ease: 'linear' }}
                 >
@@ -90,15 +94,15 @@ export default function Ingredient() {
             </div>
           </motion.div>
 
-          <p className="ingredients__desc font-figtree text-[1.28rem] mt-[20px] max-w-[295px] w-full">
+          <p className="ingredients__desc font-figtree md:text-fluid3lg text-[1.4rem] xl:mt-[20px] md:ml-0 ml-auto mt-[14px] xl:max-w-[295px] md:max-w-[196px] max-w-[200px] w-full">
             Unreservedly honest products that truly work, be kind to skin and the planet - no exceptions!
           </p>
         </div>
 
-        <div className="ingredients__wrapper relative">
+        <div className="ingredients__wrapper relative md:max-w-container">
           {/* Leaf with parallax */}
           <motion.div
-            className="media-img absolute right-[5%] top-[20%] w-[217px] -translate-x-[50%] -translate-y-[50%]"
+            className="media-img absolute right-[5%] top-[20%] xl:w-[217px] w-[145px] -translate-x-[50%] -translate-y-[50%] max-md:hidden"
             style={{
               transform: leafTransform,
               transition: 'transform 0.5s ease-out'
@@ -117,14 +121,13 @@ export default function Ingredient() {
 
           {/* Empress with parallax */}
           <motion.div
-            className="media-img absolute bottom-[7%] left-[25%] w-[217px] -translate-x-[50%] -translate-y-[50%]"
+            className="media-img absolute bottom-[7%] left-[25%] xl:w-[217px] w-[145px]  -translate-x-[50%] -translate-y-[50%] max-md:hidden"
             style={{
               transform: empressTransform,
               transition: 'transform 0.5s ease-out'
             }}
           >
             <Image
-              className="w-auto h-auto"
               src="/ingredients/ingredients-empress.png"
               alt="Empress"
               decoding="async"
@@ -137,7 +140,7 @@ export default function Ingredient() {
 
           {/* Avatar with parallax */}
           <div
-            className={`${styles.ingredientsMaskImgWrapper} ingredients__image-wrapper flex absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-[70%] h-[100%] -z-[1]`}
+            className={`${styles.ingredientsMaskImgWrapper} ingredients__image-wrapper flex md:absolute relative md:top-[50%] left-[50%] -translate-x-[50%] md:-translate-y-[50%] sm:w-[70%] w-full md:h-[100%] sm:h-[70dvh] h-[75dvh] max-[480px]:h-[55dvh] md:-z-[1]`}
           >
             <motion.div
               className={`w-[100%] h-[120%] object-cover pointer-events-none user-select-none`}
@@ -160,13 +163,14 @@ export default function Ingredient() {
           </div>
 
           {/* Cards with parallax */}
-          <div className="ingredients__firstrow flex items-start">
+          <div className="ingredients__first-row flex items-start max-[768px]:py-[12px]">
             {initialIngredients.slice(0, 2).map((card, cardIndex) => {
               const globalIndex = cardIndex
               const cardKey = `card${globalIndex + 1}` as keyof typeof transforms
               return (
                 <motion.div
                   key={cardIndex}
+                  className={`${styles.ingredientsFirstCol} max-[768px]:flex-1`}
                   style={{
                     transform: transforms[cardKey],
                     transition: 'transform 0.5s ease-out'
@@ -178,13 +182,14 @@ export default function Ingredient() {
             })}
           </div>
 
-          <div className="ingredients__secondrow flex items-start justify-end -mt-[10%]">
+          <div className="ingredients__second-row flex items-start justify-end md:-mt-[10%] max-[768px]:py-[12px]">
             {initialIngredients.slice(2, 4).map((card, cardIndex) => {
               const globalIndex = cardIndex + 2
               const cardKey = `card${globalIndex + 1}` as keyof typeof transforms
               return (
                 <motion.div
                   key={cardIndex + 2}
+                  className={`${styles.ingredientsFirstCol} max-[768px]:flex-1`}
                   style={{
                     transform: transforms[cardKey],
                     transition: 'transform 0.5s ease-out'
