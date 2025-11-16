e746a0719a72fe0a7ae39fe1f5da847f70c5ea4f
leeun <Nhunglee.221@gmail.com>
2025-11-17 02:01:03 +0700

    Update responsive explore

--- src/components/explore/ExploreTitle.tsx
+++ src/components/explore/ExploreTitle.tsx
@@ -9,19 +9,19 @@ interface ExploreTitleProps {
 const ExploreTitle = ({ isInViewSection }: ExploreTitleProps) => {
   return (
     <motion.div
-      className="explore__title block m-auto mb-[88px]"
+      className="explore__title block m-auto md:mb-[88px] min-[490px]:mb-[80px] mb-[62px]"
       variants={motionTextVariants.staggerContainer}
       initial="hidden"
       animate={isInViewSection ? 'visible' : 'hidden'}
       transition={{ duration: 1.2, ease: 'linear' }}
     >
-      <h2 className="masking-text block font-figtree text-center text-[5.8rem] font-[500] leading-[52px] -tracking-[1px] h-[58px] overflow-hidden">
+      <h2 className="masking-text block font-figtree text-center text-fluid8xl font-[600] -tracking-[1px] overflow-hidden">
         <motion.span className="word inline-block will-change-transform" variants={motionTextVariants.fadeInUp}>
           EXPLORE
         </motion.span>
       </h2>
 
-      <h2 className="masking-text block font-sentient text-center text-[7.6rem] italic font-[400] leading-[50px] -tracking-[5px] h-[76px] overflow-hidden">
+      <h2 className="masking-text block font-sentient text-center text-fluid6xl italic -tracking-[5px] overflow-hidden">
         <motion.span className="word inline-block will-change-transform" variants={motionTextVariants.fadeInUp}>
           pure potency
         </motion.span>
