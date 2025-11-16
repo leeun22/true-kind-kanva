6692c2fa1a409e2e9cb3958f7320783dc4bb2c3a
leeun <Nhunglee.221@gmail.com>
2025-11-17 04:38:12 +0700

    Create static generateStaticParams

--- src/app/journal/[handle]/page.tsx
+++ src/app/journal/[handle]/page.tsx
@@ -1,3 +1,11 @@
+export async function generateStaticParams() {
+  const handles = ['collections', 'product-name', 'product-handle']
+
+  return handles.map((handle) => ({
+    handle: handle
+  }))
+}
+
 export default function JournalHandlePage() {
   return <h1>Hello, Journal Handle Page!</h1>
 }
--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -5,14 +5,6 @@ export const metadata: Metadata = {
   description: 'Journal'
 }
 
-export async function generateStaticParams() {
-  const handles = ['collections', 'product-name', 'product-handle']
-
-  return handles.map((handle) => ({
-    handle: handle
-  }))
-}
-
 export default function JournalPage() {
   return <h1>Hello, Journal Page!</h1>
 }
