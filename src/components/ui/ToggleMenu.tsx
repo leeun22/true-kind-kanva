5dc8e326dde2cbf96a2dfb208092f55421e95ad9
leeun <Nhunglee.221@gmail.com>
2025-11-17 01:57:08 +0700

    delete

--- src/components/ui/ToggleMenu.tsx
+++
@@ -1,8 +0,0 @@
-export function ToggleMenu() {
-  return (
-    <div className="navbar-hamburger">
-      <div className="navbar-hamburger-line navbar-hamburger-line-1" />
-      <div className="navbar-hamburger-line navbar-hamburger-line-2" />
-    </div>
-  )
-}
