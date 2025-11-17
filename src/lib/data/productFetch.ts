d315e1852ebab15e426ccf34008266890e859c5a
leeun <Nhunglee.221@gmail.com>
2025-11-17 12:57:27 +0700

    Update fetching product

--- src/lib/data/productFetch.ts
+++ src/lib/data/productFetch.ts
@@ -1,16 +1,13 @@
 import { unstable_cache } from 'next/cache'
 import { Product } from '@/types/product'
 
+const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL
+
 /** Layer 1 cache function: This function ensures that the API is only called once to get the entire original data. Dữ liệu gốc chỉ được fetch từ API một lần, sau đó được lưu trong cache. */
 export const fetchAllProducts = unstable_cache(
   async (): Promise<Product[]> => {
     try {
-      const BASE_URL = process.env.NEXT_PUBLIC_API_URL
-
-      if (!BASE_URL) {
-        console.error('API URL is not defined.')
-        return []
-      }
+      const BASE_URL = `${BASE_API_URL}/products`
 
       const res = await fetch(BASE_URL, {
         cache: 'force-cache'
